4b94cd5120718ae6f4eaaf45977f29b8c9fcc917
lovelessunloveable <145833189+lovelessunloveable@users.noreply.github.com>
2023-10-14 16:10:58 +0700

    Add files via upload

--- loveless.py
+++ loveless.py
@@ -1,4 +1,6 @@
 from turtle import *
+import sys
+sys.setrecursionlimit(99999)
 from itertools import product
 def f2():
     print('x y z w')
@@ -54,3 +56,90 @@ def f12():
                 s = s.replace ('1122', '25', 1)
                 if sum(map(int,s)) == 64:
                     print (n)
+
+def f16a():
+        #sys.setrecursionlimit(2500)
+    itog1=itog2=1
+    for x1 in range(1,2024):
+        itog1=itog1*x1
+    for x2 in range(1,2021):
+        itog2=itog2*x2
+    print(itog1/itog2)
+
+def F(n):
+    if n == 1: return n
+    if n>1: return n*F(n-1)
+print(F(2023)/F(2020))
+
+def f17():
+    with open('17_4705.txt') as f:  
+        nums=[int(x) for x in f]
+        maxi=[]
+        s=[]
+       
+        for i in range(len(nums)):
+          if nums[i]%10==3:
+             maxi.append(nums[i])
+        maximum=0
+        for i in range(len(nums)-1):
+            a=abs(nums[i])%10
+            b=abs(nums[i+1])%10
+            if ((a==3) and (b!=3)) or ((a!=3) and (b==3)):
+                if (nums[i]**2+nums[i+1]**2) >= max(maxi)**2: 
+                    s.append(nums[i]+nums[i+1])
+                if nums[i]**2+nums[i+1]**2>maximum:
+                    maximum=nums[i]**2+nums[i+1]**2
+    print(len(s), maximum)
+
+def f(a,m):
+        if a>77: return m%2==0
+        if m==0: return 0
+        actions=(a+1),(a+4),(a*4)
+        steps=[f(a,m-1) for a in actions]
+        if m%2==0: return all(steps)
+        else: return any(steps)
+s20=[s for s in range(1,78) if not f(s,1) and f(s,3)]
+print(s20)
+s21=[s for s in range(1,78) if f(s,4)]
+print(min(s21))
+
+def f23(x,y):
+    if x>y or x==17:
+        return 0
+    elif x==y:
+        return 1
+    return f23(x+1,y) +f23(x*2,y)
+print(f23(1,10)*f23(10,35))
+
+def f24():
+    with open('24_4710.txt') as f:
+        s=f.readline().replace('C','S').replace('D','S').replace('F','S')
+        s=s.replace('A','G').replace('O','G')
+        s=s.replace('SG','*')
+        k=kmax=0
+        for i in s:
+            if i=='*':
+                k+=1
+                kmax=max(k,kmax)
+            else:k=0
+    print(kmax)
+
+def f25():
+    for i in range(2023,10**10,2023):
+        num=str(i)
+        if num[0]=='1' and num[2:6]=='2139' and num[-1]=='4':
+            print(i,i//2023)
+
+def f26():
+    with open('26_4712.txt') as f:
+        s=[int(x) for x in f]
+        s=sorted(s[1:],reverse=True)
+        k,mini=1,s[0]
+
+    for i in range(1,len(s)):
+        if s[i]+3<=mini:
+            mini=s[i]
+            k+=1
+    print(k,mini)
+
+f26()
